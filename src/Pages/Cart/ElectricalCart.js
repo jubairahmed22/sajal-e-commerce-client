880df8a441e76c23b2520a4df1b2cecab5726897
jubairahmed22 <jubairahmed060@gmail.com>
2024-10-28 03:17:28 +0600

    cart new design  added

--- src/Pages/Cart/ElectricalCart.js
+++ src/Pages/Cart/ElectricalCart.js
@@ -66,7 +66,7 @@ const ElectricalCart = () => {
       <section class="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
         <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
           <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
-            Shopping Cart
+            Shopping
           </h2>
 
           <div class="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
